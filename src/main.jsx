c8a40d13a92d05ef581adc5a4e68f757884ada2e
RamiroGonzalezDavila <ram_yosh222_@hotmail.com>
2023-05-18 20:33:27 -0600

    modificacion de hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, RouterProvider, createHashRouter } from 'react-router-dom'
+import About from './components/about.jsx'
+import GameDevelopment from './components/GameDevelopment.jsx'
+
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element : <About/>,
+  },
+  {
+    path:"/GameDevP",
+    element: <GameDevelopment/>,
+  },
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App/>
+    <RouterProvider router={router}/>
   </React.StrictMode>
 )
